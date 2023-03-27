7543401f855b82605c4e9a1fc94087f8d63b1bc7
Zekican <l2021012041@ogr.sdu.edu.tr>
2023-03-27 09:49:56 +0300

    UI Changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,37 +1,87 @@
 import React from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, Text } from 'react-native'
 import SearchBar from 'react-native-dynamic-search-bar'
 import Animation from '../../assets/animations/Animation'
 import Anims from '../../assets/animations/index'
 import styles from './Home.style'
 
+
+const TopContainer = () => {
+    return (
+        <View style={styles.top_container['light']}>
+            <View style={styles.animation_container['light']}>
+                {/* Hava durumuna göre gösterilecek animasyon*/}
+                <Animation source={Anims.rainy} />
+            </View>
+            <View style={styles.other_daily_container['light']}>
+                {/* Diğer saatlerdeki hava durumları */}
+            </View>
+        </View>
+    )
+}
+
+const MidContainer = () => {
+    return (
+        // Hava durumu bilgileri
+        <View style={styles.mid_container['light']}>
+            <View style={styles.mid_left['light']}>
+                <Text style={styles.degree['light']}>
+                    7 °C
+                </Text>
+            </View>
+            <View style={styles.mid_right['light']}>
+                <View style={styles.mid_right_inner_top['light']}>
+                    <Text style={styles.city['light']}>Istanbul</Text>
+                </View>
+                <View style={styles.mid_right_inner_bottom['light']}>
+                    <Text style={styles.weather_status['light']}>
+                        Yağışlı
+                    </Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const BottomContainer = () => {
+    return (
+        <View style={styles.bottom_container['light']}>
+
+        </View>
+    )
+}
+
+const Searchbar = () => {
+    return (
+        <View style={styles.search_bar_container['light']}>
+            <SearchBar
+                placeholder="Şehir ismi giriniz..."
+                onPress={() => alert("onPress")}
+                onChangeText={(text) => console.log(text)}
+            />
+        </View>
+    )
+}
+
+const Statusbar = () => {
+    return (
+        <StatusBar
+        backgroundColor={'slategray'}
+        />
+    )
+}
+
+
+
+
 export default function () {
     return (
         <View style={styles.container['light']}>
-            <StatusBar
-                backgroundColor={'slategray'}
-            />
-            <View style={styles.search_bar_container['light']}>
-                <SearchBar
-                    placeholder="Search here"
-                    onPress={() => alert("onPress")}
-                    onChangeText={(text) => console.log(text)}
-                />
-            </View>
-            <View style={styles.top_container['light']}>
-                <View style={{ flex: 4 }}>
-                    <Animation source={Anims.cloudy} />
-                </View>
-                <View style={{ flex: 1 }}>
-
-                </View>
-            </View>
-            <View style={styles.mid_container['light']}>
-
-            </View>
-            <View style={styles.bottom_container['light']}>
-
-            </View>
+            <Statusbar/>
+            <Searchbar/>
+            <TopContainer/>
+            <MidContainer/>
+            <BottomContainer/>
         </View>
     );
 };
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -4,43 +4,123 @@ export default StyleSheet.create({
     container: {
         light: {
             flex: 1,
-            backgroundColor: 'slategray',
+            backgroundColor: '#8395a7',
         },
-        dark: {
-
-        }
     },
     top_container: {
         light: {
             flexDirection: 'row',
             flex: 2,
+            marginVertical: 16,
+            marginHorizontal: 12,
         },
-        dark: {
-
+    },
+    animation_container: {
+        light: {
+            flex: 1
         }
     },
+    other_daily_container: {
+        light: {
+            flex: 0.25,
+            backgroundColor: '#303030',
+            borderRadius: 4,
+        }
+    },
+
+    /* Mid */
     mid_container: {
         light: {
             flex:1,
+            flexDirection: 'row',
         },
-        dark: {
+    },
 
+    /* Mid Left */
+
+    mid_left: {
+        light: {
+            flex:0.5,
+            backgroundColor: '#303030',
+            margin: 8,
+            borderRadius: 8,
+            justifyContent: 'center',
+            alignItems: 'center',
         }
     },
+
+    degree: {
+        light: {
+            color: 'white',
+            fontSize: 36,
+        }
+    },
+
+
+    /* Mid Right */
+
+    mid_right: {
+        light: {
+            flex: 1,
+            flexDirection: 'column',
+            margin: 8,
+            borderRadius: 8,
+        }
+    },
+
+    /* Mid right inner top */
+    mid_right_inner_top: {
+        light: {
+            flex: 1,
+            backgroundColor: '#16a085',
+            borderTopStartRadius: 4,
+            borderTopEndRadius: 4,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    },
+
+    city: {
+        light: {
+            color: 'white',
+            fontSize: 24,
+            fontWeight: 'bold',
+        }
+    },
+
+    weather_status: {
+        light: {
+            color:'white',
+            fontSize: 32,
+        }
+    },
+
+    mid_right_inner_bottom: {
+        light: {
+            flex: 2,
+            backgroundColor: '#303030',
+            borderBottomStartRadius: 4,
+            borderBottomEndRadius: 4,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    },
+
+
+
+
+    /* Bottom */
     bottom_container: {
         light: {
             flex:1,
+            backgroundColor: '#303030',
+            margin: 8,
+            borderRadius: 8,
         },
-        dark: {
-
-        }
     },
     search_bar_container:{
         light: {
-            marginTop:10,
-        },
-        dark: {
-
-        }   
+            marginTop:8,
+        },  
     },
 })
