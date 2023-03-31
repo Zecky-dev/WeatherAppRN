8c625f4c5ed367624f654291482879515faeb73b
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2023-03-31 21:21:06 +0300

    Gün döngüsü oluşturuldu. Çeşitli animasyonlar eklendi.

--- src/assets/animations/index.js
+++ src/assets/animations/index.js
@@ -9,7 +9,11 @@ const assets = {
         sunny: require('./files/sunny.json'),
         thunder: require('./files/thunder.json'),
         loading: require('./files/loading.json'),
-        error: require('./files/error.json')
+        error: require('./files/error.json'),
+        clear_sky_night: require('./files/clear_sky_night.json'),
+        partly_cloudy_night: require('./files/partly_cloudy_night.json'),
+        rainy_night: require('./files/rainy_night.json'),
+        snowy_night: require('./files/snowy_night.json'),
     }
 }
 
